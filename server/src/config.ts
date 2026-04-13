2b15ae5367ce0b1465ecf05afaf6a241158fa405
Renan Oliveira <rotrenanoliveira@gmail.com>
2026-04-13 15:33:36 -0300

    chore: define app config

---
+++ server/src/config.ts
@@ -0,0 +1,7 @@
+export const BCRYPT_ROUNDS = 10
+export const MAX_LOGIN_ATTEMPTS = 3
+export const LOCKOUT_MINUTES = 15
+export const PASSWORD_RESET_TTL_MIN = 60
+export const TWO_FACTOR_PENDING_TTL_MIN = 10
+export const RATE_MAX = 100
+export const RATE_TIME_WINDOW_MS = 60_000
